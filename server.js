fbcc6f814d37776b53943fa60e9208847761a9a3
keneni45 <kenotoneaga@gmail.com>
2024-12-14 02:10:47 -0800

    fix production error

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -6,25 +7,24 @@ const Vehicle = require('./models/vehicle');
 
 const app = express();
 
+//const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Failed to connect to MongoDB:", err));
+
+// Middleware
+app.use(express.json());
+
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
 
 
-//connect to mongo atlas
-mongoose.connect(
-  'mongodb+srv://keno:W82cxfoL3aY3EoQT@cluster0.dnokv.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true }
-)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => {
-    console.error('MongoDB Connection Error:', err.message);
-    process.exit(1); // Exit the process if connection fails
-  });
-
-
-// Routes
 // Fetch all vehicles
 app.get('/vehicles', async (req, res) => {
   const vehicles = await Vehicle.find();
@@ -46,6 +46,6 @@ app.put('/vehicles/:id', async (req, res) => {
   res.json(vehicle);
 });
 
-// Start server
-const PORT = 5000;
+
+//const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
